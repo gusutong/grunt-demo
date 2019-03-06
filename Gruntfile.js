2bee9e577918bb597601131b154603b76a2dbb18
gusutong <562656798@qq.com>
2019-03-06 19:50:33 +0800

    csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,12 @@
-ule.exports = function(grunt){
+module.exports = function(grunt){
   grunt.initConfig({
-    htmlhint:{
-      html:{
-        options:{
-          htmlhintrc:'.htmlhintrc'    
-        },
-        src:['*.html']   
-      }          
-    }            
+    csslint:{
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      src: ['*.css']   
+    }         
   });
-  grunt.loadNpmTasks('grunt-htmlhint');
-  grunt.registerTask('default',['htmlhint']);
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.registerTask('default',['csslint']);
 };
