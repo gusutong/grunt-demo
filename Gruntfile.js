6dce357d729d28ea4b419d39c1e225d05a7739cf
gusutong <562656798@qq.com>
2019-03-12 14:35:01 +0800

    htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,17 @@
-module.exports = function (grunt) {
+module.exports = function(grunt){
   grunt.initConfig({
-    mochacli: {
-      options: {
-        reporter: 'spec',
-        bail: true 
-      },
-      all: ['test/*.js']                   
-    },
-    mocha_istanbul: {
-      coverage: {
-        src: 'test'          
-      }    
-    },
-    istanbul_check_coverage: {
-      default: {
-        options: {
-          coverageFolder: 'coverage*',
-          check: {
-            lines: 90,
-            statements: 90                                    
-          }               
-        }    
-      }   
-    }  
+    htmlmin:{
+      dev: {     // Another target
+        options:{
+          collapseWhitespace:true,
+          preservelLineBreaks:false
+        }
+        files: {
+          'dist/index.html': 'src/index.html'
+        }
+      }                   
+    }            
   });
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
-        
-  grunt.registerTask('default', ['mochacli']);   
-  grunt.registerTask('cover', ['mocha_istanbul']);
-  grunt.registerTask('check-cover', ['istanbul_check_coverage']);
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.registerTask('default',['htmlmin']);
 };
