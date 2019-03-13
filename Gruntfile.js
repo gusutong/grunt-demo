34e6607b18e56d49c9b0f4ecbf96608c455f3f6c
gusutong <562656798@qq.com>
2019-03-13 16:33:49 +0800

    http-api-test

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,19 @@ module.exports = function(grunt){
   grunt.initConfig({
     mochacli:{
       options:{
-
+        reporter: 'spec',
+        bail: true
+      },
+      all: ['test/*.js']
+    },
+    run:{
+      api:{
+       options:{ wait: false },
+       args: ['./app.js']
       }
-    }    
+    },
   });
-  grunt.loadNpmTasks('grunt-spritesmith');
-  grunt.registerTask('default',['sprite']);
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-run');
+  grunt.registerTask('default',['run','mochacli','stop']);
 };
